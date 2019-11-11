79c3c482e30cde13f2d68410847248321d9e901a
silex189 <antoniojm89@gmail.com>
2019-11-11 19:18:27 +0100

    GET

--- RICK-Y-MORTY/src/index.js
+++ RICK-Y-MORTY/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './styles.css';
 import logo from "./images/logo.png";
+import Loader from './Components/Loader';
 
 function CharacterCard(props) {
   const { character } = props;
@@ -21,13 +22,13 @@ function CharacterCard(props) {
 
 class App extends React.Component {
   state = {
+    nextPage: 1,
     loading: true,
     error: null,
     data: {
       info: {},
       results: []
-    },
-    nextPage: 1
+    }
   };
 
   componentDidMount() {
@@ -74,7 +75,12 @@ class App extends React.Component {
             ))}
           </ul>
 
-          {this.state.loading && <p className="text-center">Loading...</p>}
+          {this.state.loading && (
+            <div className="loader">
+              <Loader />
+            </div>
+          )}
+        
 
           {!this.state.loading && this.state.data.info.next && (
             <button onClick={() => this.fetchCharacters()}>Load More</button>
--- platzi-badges/src/pages/Badges.js
+++ platzi-badges/src/pages/Badges.js
@@ -4,8 +4,80 @@ import { Link } from 'react-router-dom';
 import './styles/Badges.css';
 import confLogo from '../images/badge-header.svg'
 import BadgesList from '../components/BadgesList';
+import api from '../api';
 
 class Badges extends React.Component {
+  state = { 
+    loading: true,
+    error: null,
+    data: undefined
+  };
+
+  componentDidMount () {
+    this.fetchData()
+  }
+
+  fetchData = async () => {
+    this.setState({ loading: true, error: null })
+
+    try {
+      const data = await api.badges.list();
+      this.setState({ loading: false, data: data });
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  }
+
+
+  render() {
+    if (this.state.loading === true) {
+      return 'Loading...';
+    }
+    if (this.state.error) {
+      return `Error: ${this.state.error.message}`;
+    }
+
+    console.log('2/4. render()')
+    return (
+      <React.Fragment>
+        <div className="Badges">
+          <div className="Badges__hero">
+            <div className="Badges__container">
+              <img 
+                className="Badges_conf-logo"
+                src={confLogo} 
+                alt="Conf Logo"></img>
+            </div>
+          </div>
+        </div>
+
+        <div className="Badges__container">
+          <div className="Badges__buttons">
+            <Link to="/Badges/new" className="btn btn-primary">
+              New Badge
+            </Link>
+          </div>
+
+          <div className="Badge__List">
+            <div className="Badges__container">
+              <BadgesList badges={this.state.data} />
+              
+              
+              
+
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Badges;
+
+
+
+/* 
 
 
   constructor(props) {
@@ -76,41 +148,4 @@ class Badges extends React.Component {
     clearTimeout(this.timeoutId)
   }
 
-  render() {
-    console.log('2/4. render()')
-    return (
-      <React.Fragment>
-        <div className="Badges">
-          <div className="Badges__hero">
-            <div className="Badges__container">
-              <img 
-                className="Badges_conf-logo"
-                src={confLogo} 
-                alt="Conf Logo"></img>
-            </div>
-          </div>
-        </div>
-
-        <div className="Badges__container">
-          <div className="Badges__buttons">
-            <Link to="/Badges/new" className="btn btn-primary">
-              New Badge
-            </Link>
-          </div>
-
-          <div className="Badge__List">
-            <div className="Badges__container">
-              <BadgesList badges={this.state.data} />
-              
-              
-              
-
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
-}
-
-export default Badges;
+  */
