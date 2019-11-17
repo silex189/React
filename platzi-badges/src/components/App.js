ef572ab2da99cbc0d6fa4bef51e2c9f86f5ba38e
silex189 <antoniojm89@gmail.com>
2019-11-17 19:19:09 +0100

    delete

--- platzi-badges/src/components/App.js
+++ platzi-badges/src/components/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 
 import Layout from '../components/Layout';
 import BadgeNew from '../pages/BadgeNew';
+import BadgeDetails from '../pages/BadgeDetailsContainer';
 import BadgeEdit from '../pages/BadgeEdit';
 import Badges from '../pages/Badges';
 import NotFound from '../pages/NotFound';
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" component={Badges} />
           <Route exact path="/badges" component={Badges} />
           <Route exact path="/badges/new" component={BadgeNew} />
+          <Route exact path="/badges/:badgeId" component={BadgeDetails} />
           <Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
           <Route component={NotFound} />
         </Switch>
